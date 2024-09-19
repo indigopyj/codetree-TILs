0e64464f7dd1001afe0d7b0dc221853d5c6b6b7b
Yeojeong Park <indigopyj@gmail.com>
2024-09-19 21:53:29 +0900

    Title: 코드트리 채점기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240919/코드트리 채점기/codetree-judger.py
+++ 240919/코드트리 채점기/codetree-judger.py
@@ -7,18 +7,17 @@ class Domain():
         self.domain = domain
         self.start = start
         self.gap = gap
-        self.waiting_queue = []
+        self.waiting_queue = [] # 도메인마다 queue 생성
         self.now_judging = False
     
-    def hq_push(self, p, t, u):
+    def hq_push(self, p, t, u): 
         for item in self.waiting_queue:
             if u == item[-1]:
-                # print(u, self.waiting_queue)
                 return False
         heapq.heappush(self.waiting_queue, (p, t, u))
         return True
     
-    def check_judge(self, curr_t):
+    def check_judge(self, curr_t): # 채점 가능한지 체크
         if self.now_judging:
             return False
         if curr_t < self.start + 3 * self.gap:
@@ -28,40 +27,13 @@ class Domain():
     def hq_pop(self):
         return heapq.heappop(self.waiting_queue)
     
-
     def print_(self):
         print(self.start, self.gap, self.now_judging, self.waiting_queue)
 
-
-    
-MAX_T = 1000001
-MAX_N = 50001
 Q = int(input())
-cmd_list = []
-judge_cmd_list = []
-hq = []
-waiting_domain = set()
+waiting_domain = set() # waiting queue에 겹치는 도메인 있는지 확인
 domain_dict = defaultdict(Domain)
-job_judger = []
-
-def is_available(curr_t, curr_domain, N):
-    if curr_domain in job_judger[1:N+1]:
-        return False
-    start_t, gap = domain_dict[curr_domain]
-    if curr_t < start_t + 3 * gap:
-        return False
-    # print("true")
-    return True
-
-
-# def judger_exist(N):
-#     job_judger
-#     for i, jj in enumerate(job_judger[:N+1]):
-#         if i > 0 and jj == '':
-#             return i
-#     return -1
-
-
+job_judger = [] # 놀고있는 judger를 뱉어줄 priority queue
 ans = 0
 
 for q in range(Q):
@@ -82,50 +54,49 @@ for q in range(Q):
         # init. job judger
         for i in range(1, N+1):
             heapq.heappush(job_judger, i) # number
-        judging_domain = ['' for _ in range(N+1)]
+        judging_domain = ['' for _ in range(N+1)] # 각 채점기가 채점중인 domain 기록
 
     elif cmd == 200:
         # request scoring, 200 t p u
         t, p = map(int, inputline[1:-1])
         u = inputline[-1]
         domain = u.split('/')[0]
-        check = domain_dict[domain].hq_push(p, t, u)
-        if check:
+        check = domain_dict[domain].hq_push(p, t, u) # push가 안될 경우(즉, domain이 겹치는 경우) return false
+        if check: # push가 되는 경우 채점 대기 큐에 들어감
             ans += 1
-        waiting_domain.add(domain)
+        waiting_domain.add(domain) # 현재 대기중인 도메인 추가
 
     elif cmd == 300:
         # try scoring
         t = int(inputline[1])
-        tmp_hq = []
-        for d_i, domain_info in domain_dict.items():
-            if domain_info.check_judge(t):
-                if domain_info.waiting_queue:
+        tmp_hq = [] # 먼저 각 도메인 별로 채점 가능한 애들만 모은 뒤, 거기서 우선순위를 매길 예정
+        for d_i, domain_info in domain_dict.items(): # 각 도메인 별로 큐를 확인해서 채점 가능한 애들만 모으자
+            if domain_info.check_judge(t): # 대기중인 도메인 중에 채점 가능한 원소가 있다면
+                if domain_info.waiting_queue: # waiting queue가 비어있지 않다면
                     priority, input_time, url = domain_info.waiting_queue[0]
                     heapq.heappush(tmp_hq, (priority, input_time, d_i))
-        if tmp_hq and job_judger:
+        if tmp_hq and job_judger: # 채점 가능한 애들이 존재하고, 지금 작업 가능한 채점기가 있다면
             j_id = heapq.heappop(job_judger)
-            # print("job starts: ", tmp_hq[0], j_id)
-            _, _, d_i = tmp_hq[0]
-            # print("job start: ", j_id, domain_dict[d_i])
-            waiting_domain.discard(d_i)
+            _, _, d_i = tmp_hq[0] # 제일 우선순위가 높은 애를 꺼내자
+            # 대기중인 큐에서 해당 노드에 대한 정보를 제거함
+            waiting_domain.remove(d_i)
             domain_dict[d_i].hq_pop()
+            # 채점기 j_id 가 채점 시작
             judging_domain[j_id] = d_i
             domain_dict[d_i].start = t
             domain_dict[d_i].now_judging = True
             ans -= 1
-            # domain_dict[d_i].print_()
             
     elif cmd == 400:
         # end scoring
         t, j_id = map(int, inputline[1:])
         d_i = judging_domain[j_id]
-        if d_i == '':
+        if d_i == '': # 만약 j_id 채점기가 채점하고 있는 것이 없다면 패스
             continue
+        # j_id가 채점하고 있던 노드의 정보 갱신
         domain_dict[d_i].gap = t - domain_dict[d_i].start 
         domain_dict[d_i].now_judging = False
-        # print("job ends : ", j_id, domain_dict[d_i].gap)
-        # domain_dict[d_i].print_()
+        # 놀고 있는 judger로 다시 추가시키기
         heapq.heappush(job_judger, j_id)
         judging_domain[j_id] = ''
 
@@ -133,54 +104,3 @@ for q in range(Q):
         # print num of tasks in queue
         t = int(inputline[1])
         print(ans)
-    
-    
-
-
-# for jc in judge_cmd_list:
-#     # print(jc)
-#     cmd, curr_t, J_id = jc
-#     if cmd == 300:
-#         curr_p, start_t, curr_domain, curr_uid = hq[0]
-#         curr_url = (curr_domain + '/' + str(curr_uid))
-#         if curr_url in url_list[start_t+1:curr_t]:
-#             cmd_list.append((222, start_t, -1, -1, -1)) # pass adding to queue
-#             # print(222, start_t)
-#             heapq.heappop(hq)
-#             continue
-#         tmp_hq = []
-#         while hq:
-#             curr_p, start_t, curr_domain, curr_uid = heapq.heappop(hq)
-#             if not is_available(curr_t, curr_domain, N):
-#                 tmp_hq.append((curr_p, start_t, curr_domain, curr_uid))
-#                 continue
-#             if job_judger:
-#                 J_id = heapq.heappop(job_judger)
-#                 judging_domain[J_id] = curr_domain
-#                 domain_dict[curr_domain][0] = curr_t
-#                 # print(333, curr_t, J_id, curr_domain, job_judger[:N+1])
-#                 cmd_list.append((333, curr_t, J_id, -1, -1))
-#                 url_list[start_t] = ''
-#             break
-#         for tmp in tmp_hq:
-#             heapq.heappush(hq, tmp)
-#     else:
-#         domain = judging_domain[J_id]
-#         if domain == '':
-#             continue
-#         domain_dict[domain][1] = curr_t - domain_dict[domain][0]
-#         # print(400, J_id, domain, domain_dict[domain])
-#         heapq.heappush(job_judger, J_id)
-
-# cmd_list = sorted(cmd_list, key=lambda x:x[1])
-# # print(cmd_list)
-# total_tasks = 0
-# for c in cmd_list:
-#     if c[0] == 100 or c[0] == 200:
-#         total_tasks += 1
-#     elif c[0] == 222:
-#         total_tasks -= 1
-#     elif c[0] == 333:
-#         total_tasks -= 1
-#     elif c[0] == 500:
-#         print(total_tasks)
