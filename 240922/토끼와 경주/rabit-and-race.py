5c00dcff86d89c1fcb375ff8812af549cebd5e37
Yeojeong Park <indigopyj@gmail.com>
2024-09-22 00:39:11 +0900

    Title: 토끼와 경주, Time: 2133ms, Memory: 30MB, Status: Passed - CodeTree

--- 240922/토끼와 경주/rabit-and-race.py
+++ 240922/토끼와 경주/rabit-and-race.py
@@ -1,129 +1,123 @@
-제대로 모든 토끼를 반복문으로 불러와서 점수를 더해주면 시간초과가 발생한다
+import heapq 
+from collections import defaultdict
 
-import sys
-from heapq import heappush, heappop
-
-input = sys.stdin.readline
-
-# d에 이동거리, point에 점수를 dict[id]로 저장
 q = int(input())
-d, point = {}, {}
-info = []
-# cnt에는 추가 점수를 저장하고 마지막에 한번에 더해준다
-cnt = 0
+
+MAX_P = 2000
+
+n, m, p = -1,-1, -1
+
+# id에 해당하는 이동거리 저장 
+r_dist = {}
+# id에 해당하는 index에 점수 저장 -> max score 구할 때 max(scores)로 구하기 위함 
+scores = [0] * (MAX_P+1)
+
+# 가장 우선 순위 높은 토끼 구할 때 사용할 힙 배열 
+r_heap = []
+# id에 해당하는 index 저장
+r_index = {}
+# index에 해당하는 id 저장 
+r_id = {}
+
+def build(data):
+    global n,m,p
+    
+    n = data[1]
+    m = data[2]
+    p = data[3]
+    
+    data = data[4:]
+    
+    for i in range(p):
+        id = data[i*2]
+        d = data[i*2+1]
+        
+        # id 별 이동 거리 저장 
+        r_dist[id] = d 
+        # id 별 index 저장 
+        r_index[id] = i
+        # index 별 id 저장 
+        r_id[i] = id 
+        # 우선순위 조건에 따라 heap에 삽입 
+        # 총 점프 횟수, 행+열, 행, 열, 고유번호
+        heapq.heappush(r_heap, (0,0,0,0,id))
+        
+def get_point(r_id, r_score):
+    index = r_index[r_id]
+    # 이동한 토끼 제외한 토끼들 r+c 만큼 점수 더하기 
+    for i in range(p):
+        if i != index :
+            scores[i] += r_score
+
+def get_top_point(r_id,s):
+    # 이동한 토끼 s 만큼 점수 더하기 
+    index = r_index[r_id]
+    scores[index] += s 
+    
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+
+def run(data):
+    global n,m 
+    k = data[1]
+    s = data[2]
+    # k번의 턴 동안 뽑혔던 적이 있던 토끼를 고를 때 사용 
+    r_select_heap = []
+    
+    for _ in range(k):
+        r_jump, r_sum, r_x, r_y, r_id = heapq.heappop(r_heap)
+        
+        # 상하좌우 이동 후 위치 
+        positions = []
+        for i in range(4):
+            nx = r_x + dx[i] * r_dist[r_id]
+            ny = r_y + dy[i] * r_dist[r_id]
+            # 격자를 벗어났을 때 
+            if nx < 0 or nx >= n or ny < 0 or ny >= m :
+                nx %= 2*(n-1)
+                ny %= 2*(m-1)
+                nx = min(nx, 2*(n-1)-nx) 
+                ny = min(ny, 2*(m-1)-ny) 
+            # 행+열, 행, 열 -> 큰 순이므로 마이너스로 변환해서 삽입 
+            heapq.heappush(positions, (-(nx+ny), -nx, -ny))
+            
+        # 우선 순위가 높은 칸으로 이동 
+        r_s, x, y = heapq.heappop(positions)
+        
+        # 해당 칸으로 이동 
+        heapq.heappush(r_heap, (r_jump+1, -r_s, -x, -y, r_id))
+        # 뽑혔던 토끼 리스트에 추가 
+        # 행+열, 행, 열, 고유 번호 -> 큰 순이므로 그대로 마이너스
+        heapq.heappush(r_select_heap, (x+y, x, y, -r_id))
+        
+        # 이동한 토끼 제외한 토끼들 점수 얻기 
+        get_point(r_id,-(x+y) + 2)
+    
+    # k번의 턴 동안 한번이라도 뽑혔던 적이 있던 토끼 중 가장 우선순위가 높은 토끼 
+    r_s, x, y, r_id = heapq.heappop(r_select_heap)
+    r_id = -r_id 
+    # s 만큼 점수 더하기 
+    get_top_point(r_id,s)
+    
+def change_dist(data):
+    pid = data[1]
+    l = data[2]
+
+    r_dist[pid] *= l
+
+def top_score():
+    print(max(scores))
+    
 for _ in range(q):
-    r = list(map(int, input().split()))
-    if r[0] == 100:
-        n, m, p = r[1], r[2], r[3]
-        r = r[4:]
-        for i in range(p):
-            # 각 토끼의 이동거리를 저장하고 점수는 0으로 초기화
-            # 우선순위큐에 정렬할 순서에 맞춰서 값을 넣어준다
-            # 좌표 시작점이 [1, 1]인 점 주의
-            d[r[2 * i]] = r[2 * i + 1]
-            point[r[2 * i]] = 0
-            heappush(info, [0, 2, 1, 1, r[2 * i]])
-
-    elif r[0] == 200:
-        k, s = r[1], r[2]
-        cand2 = []
-
-        for _ in range(k):
-            # 조건에 맞는 이동할 토끼를 우선수위큐에서 꺼낸다
-            jump, _, x, y, id = heappop(info)
-            tx, ty = x, y
-            dist = d[id]
-            cand = []
-
-            # 상하좌우 이동을 잘 구현하고 도착좌표들을 cand에 저장
-            # up
-            nx = x - dist
-            if nx <= 0:
-                a, b = divmod(abs(dist - x + 1), n - 1)
-                if a % 2 == 0:
-                    if b == 0:
-                        nx = 1
-                    else:
-                        nx = b + 1
-                else:
-                    if b == 0:
-                        nx = n
-                    else:
-                        nx = n - b
-            cand.append([nx, y])
-
-            # down
-            nx = x + dist
-            if nx > n:
-                a, b = divmod(dist - (n - x), n - 1)
-                if a % 2 == 0:
-                    if b == 0:
-                        nx = n
-                    else:
-                        nx = n - b
-                else:
-                    if b == 0:
-                        nx = 1
-                    else:
-                        nx = b + 1
-            cand.append([nx, y])
-
-            # left
-            ny = y - dist
-            if ny <= 0:
-                a, b = divmod(abs(dist - y + 1), m - 1)
-                if a % 2 == 0:
-                    if b == 0:
-                        ny = 1
-                    else:
-                        ny = b + 1
-                else:
-                    if b == 0:
-                        ny = m
-                    else:
-                        ny = m - b
-            cand.append([x, ny])
-
-            # right
-            ny = y + dist
-            if ny > m:
-                a, b = divmod(dist - (m - y), m - 1)
-                if a % 2 == 0:
-                    if b == 0:
-                        ny = m
-                    else:
-                        ny = m - b
-                else:
-                    if b == 0:
-                        ny = 1
-                    else:
-                        ny = b + 1
-            cand.append([x, ny])
-
-            # cand에 있는 도착좌표 후보들을 오름차순으로 정렬
-            cand.sort(key=lambda x: (x[0] + x[1], x[0], x[1]), reverse=True)
-            x, y = cand[0]
-
-            # 이동하지 않은 토끼들만 점수를 얻는다 -> 이동한 토끼만 점수를 잃는다로 바꿔서 구현
-            # cnt에 얻는 점수를 계속 더해주고 마지막 점수 최대값 결과에 더해준다
-            point[id] -= x + y
-            cnt += x + y
-
-            # 우선순위큐에 점프횟수, 이동좌표가 변경된 토끼를 넣어준다
-            # cand2에 이동한 토끼를 저장
-            heappush(info, [jump + 1, x + y, x, y, id])
-            cand2.append([x, y, id])
-
-        # 이동한 토끼들을 오름차순으로 정렬해서 조건에 맞는 토끼의 점수를 더해준다
-        cand2.sort(key=lambda x: (x[0] + x[1], x[0], x[1], x[2]), reverse=True)
-        id = cand2[0][-1]
-        point[id] += s
-
-    elif r[0] == 300:
-        # 조건대로 이동거리 수정
-        id, l = r[1], r[2]
-        d[id] *= l
-
-    else:
-        # 점수의 최대값을 구해주고 추가점수를 더해준다
-        print(max(point.values()) + cnt)
+    data = list(map(int,input().split()))
+    
+    q_type = data[0]
+    
+    if q_type == 100 :
+        build(data)
+    if q_type == 200 :
+        run(data)
+    if q_type == 300 :
+        change_dist(data)
+    if q_type == 400 :
+        top_score()
