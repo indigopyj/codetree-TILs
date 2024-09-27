088f7885661a416ad037eed00877f88732f7d924
Yeojeong Park <indigopyj@gmail.com>
2024-09-27 16:51:28 +0900

    Title: 산타의 선물 공장 2, Time: 750ms, Memory: 37MB, Status: Wrong Answer - CodeTree

--- 240927/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 240927/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -1,133 +1,139 @@
 from collections import deque, defaultdict
 import math
-
+MAX_M = 100001
+MAX_N = 100001
 q = int(input())
 
 inputline = list(map(int, input().split()))
 
 n, m = inputline[1:3]
-fronts = dict()
-backs = dict()
-n_stuff = defaultdict(int)
-stuff_info = dict()
+prevs = [0] * MAX_M
+nexts = [0] * MAX_M
+heads = [0] * MAX_N
+tails = [0] * MAX_N
+n_boxes = [0] * MAX_N
 belts = dict()
+
 for i in range(1, n+1):
     belts[i] = []
 
-# for i in range(1, n+1):
-#     belts[i] = deque()
-for i in range(1, m+1):
-    n_belt = inputline[2+i]
-    stuff_info[i] = -3
-    belts[n_belt].append(i)
+for s_id in range(1, m+1):
+    b_id = inputline[2+s_id]
+    belts[b_id].append(s_id)
+
+for i in range(1, n+1):
+    heads[i] = belts[i][0] if len(belts[i]) > 0 else 0
+    tails[i] = belts[i][-1] if len(belts[i]) > 0 else 0
+    n_boxes[i] = len(belts[i])
+    for j in range(n_boxes[i]-1):
+        box = belts[i][j]
+        nexts[box] = belts[i][j+1]
+        next_box = belts[i][j+1]
+        prevs[next_box] = box
+        
+
+def move(m_src, m_dst):
+    if n_boxes[m_src] == 0:
+        print(n_boxes[m_dst])
+        return
     
-    if len(belts[n_belt]) >= 2:
-        a = belts[n_belt][-2]
-        stuff_info[i] = a - 2
-        stuff_info[a] += 2 
-        stuff_info[a] += 2*belts[n_belt][-1]
+    origin_dst_head = heads[m_dst]
+    origin_src_tail = tails[m_src]
+    heads[m_dst] = heads[m_src]
+    nexts[origin_src_tail] = origin_dst_head
+    prevs[origin_dst_head] = origin_src_tail
+    n_boxes[m_dst] += n_boxes[m_src]
 
-# for n_belt, belt in belts.items():
-#     n_stuff[n_belt] = len(belt)
-#     if len(belt) != 0:
-#         fronts[n_belt] = belts[0]
-#         backs[n_belt] = belts[-1]
+    n_boxes[m_src] = 0
+    heads[m_src], tails[m_src] = 0, 0
 
-def update_info(src, new_before=-1, new_behind=-1):
-    stuff_info[src] = new_before+ 2*new_behind
+    print(n_boxes[m_dst])
 
-# print(belts)
-# print(stuff_info)
+def remove_head(b_id):
+    if n_boxes[b_id] == 0:
+        return 0
+    if n_boxes[b_id] == 1:
+        heads[b_id] = 0
+        origin_head = heads[b_id]
+        nexts[origin_head] = prevs[origin_head] = 0
+        n_boxes[b_id] = 0
+        return origin_head
+    origin_head = heads[b_id]
+    new_head = nexts[origin_head]
+    heads[b_id] = new_head
+    nexts[origin_head] = 0
+    prevs[new_head] = 0
+    n_boxes[b_id] -= 1
+    return origin_head
+
+def push_head(b_id, new_head):
+    if new_head == 0:
+        return
+    if n_boxes[b_id] == 0:
+        heads[b_id] = new_head
+        tails[b_id] = new_head
+        n_boxes[b_id] += 1
+        return
+
+    origin_head = heads[b_id]
+    prevs[origin_head] = new_head
+    nexts[new_head] = origin_head
+    heads[b_id] = new_head
+    n_boxes[b_id] += 1
+ 
+def change_head(m_src, m_dst):
+    src_head = heads[m_src] # 0
+    dst_head = heads[m_dst] # 2
+    # print(src_head, dst_head)
+    remove_head(m_src)
+    remove_head(m_dst)
+    # print(heads[m_src], heads[m_dst])
+
+    push_head(m_src, dst_head)
+    push_head(m_dst, src_head)
+    # print(heads[m_src], heads[m_dst])
+    print(n_boxes[m_dst])
+    
+def divide(m_src, m_dst):
+    if n_boxes[m_src] == 1:
+        print(n_boxes[m_dst])
+        return
+
+    N = math.floor(n_boxes[m_src] / 2)
+    boxes = []
+    for i in range(N):
+        elem = remove_head(m_src)
+        boxes.append(elem)
+    for box in boxes:
+        push_head(m_dst, box)
+    print(n_boxes[m_dst])
+
 
 for _ in range(q-1):
     inputline = list(map(int, input().split()))
-    # print("cmd: ", inputline)
-    if inputline[0] == 200:
-        # move stuff
+    cmd = inputline[0]
+    if cmd == 200:
         m_src, m_dst = inputline[1:]
-        if len(belts[m_src]) != 0:
-            
-            src_last = belts[m_src][-1] 
-            trg_first = belts[m_dst][0] if len(belts[m_dst]) != 0 else -1
-            update_info(src_last, belts[m_src][-2] if len(belts[m_src]) >= 2 else -1, trg_first)
-            update_info(trg_first, src_last, belts[m_dst][1] if len(belts[m_dst]) >= 2 else -1)
-            
-            belts[m_dst] = belts[m_src] + belts[m_dst]
-            belts[m_src] = []
-            # fronts[m_src] = -1
-            # backs[m_src] = -1
-            # fronts[m_trg] = belts[0]
-        print(len(belts[m_dst]))
-            
-
-    elif inputline[0] == 300:
-        # change front stuff
-        # print("before change")
-        # print(belts[m_src])
-        # print(belts[m_dst])
+        move(m_src, m_dst)
+    elif cmd == 300:
         m_src, m_dst = inputline[1:]
-        
-        src_first = belts[m_src][0] if len(belts[m_src]) != 0 else -1
-        trg_first = belts[m_dst][0] if len(belts[m_dst]) != 0 else -1
-
-        if len(belts[m_src]) > 0:
-            del belts[m_src][0]
-        if len(belts[m_dst]) > 0:
-            del belts[m_dst][0]
-        if src_first != -1:
-            belts[m_dst].insert(0, src_first)
-        if trg_first != -1:
-            belts[m_src].insert(0, trg_first)
-        if len(belts[m_dst]) != 0:
-            update_info(belts[m_dst][0], -1, belts[m_dst][1] if len(belts[m_dst]) >= 2 else -1)
-            if len(belts[m_dst]) > 1:
-                update_info(belts[m_dst][1], belts[m_dst][0], belts[m_dst][2] if len(belts[m_dst]) >= 3 else -1)
-
-        if len(belts[m_src]) != 0:
-            update_info(belts[m_src][0], -1, belts[m_src][1] if len(belts[m_src]) >= 2 else -1)
-            if len(belts[m_src]) > 1:
-                update_info(belts[m_src][1], belts[m_src][0], belts[m_src][2] if len(belts[m_src]) >= 3 else -1)
-        # print("after change")
-        # print(belts[m_src])
-        # print(belts[m_dst])
-        print(len(belts[m_dst]))
-        
-        
-
-    elif inputline[0] == 400:
-        # split stuff
+        change_head(m_src, m_dst)
+    elif cmd == 400:
         m_src, m_dst = inputline[1:]
-        n = len(belts[m_src])
-        slice_idx = math.floor(n / 2)
-        if slice_idx == 0:
-            print(len(belts[m_dst]))
-            continue
-        sliced_src = belts[m_src][:slice_idx]
-        rest_src = belts[m_src][slice_idx:]
-        src_last = sliced_src[-1]
-        trg_first = belts[m_dst][0] if len(belts[m_dst]) != 0 else -1
-        update_info(src_last, sliced_src[-2] if len(sliced_src) >= 2 else -1, trg_first)
-        update_info(trg_first, src_last, belts[m_dst][1] if len(belts[m_dst]) >= 2 else -1)
-        if len(rest_src) > 0:
-            update_info(rest_src[0], -1, rest_src[1] if len(rest_src) >= 2 else -1)
-        belts[m_dst] = sliced_src + belts[m_dst]   
-        belts[m_src] = rest_src
-
-
-        print(len(belts[m_dst]))
-
-    elif inputline[0] == 500:
-        # get stuff info
+        divide(m_src, m_dst)
+    elif cmd == 500:
         p_num = inputline[1]
-        print(stuff_info[p_num])
-    else:
-        # get belt info
-        b_num = inputline[1]
-        a,b,c = -1,-1,0
-        if len(belts[b_num]) > 0 :
-            a = belts[b_num][0]
-            b = belts[b_num][-1]
-            c = len(belts[b_num])
-        res = a + 2*b + 3*c
+        a = prevs[p_num] if prevs[p_num] != 0 else -1
+        b = nexts[p_num] if nexts[p_num] != 0 else -1
+        res = a + 2*b
         print(res)
-    # print(stuff_info)
+    else:
+        b_num = inputline[1]
+        if n_boxes[b_num] == 0:
+            res = -3
+        else:
+            # a = heads[p_num] if prevs[p_num] != 0 else -1
+            # b = nexts[p_num] if nexts[p_num] != 0 else -1
+            # print(heads[b_num], tails[b_num], n_boxes[b_num])
+            res = heads[b_num] + 2*tails[b_num] + 3*n_boxes[b_num]
+        print(res)
