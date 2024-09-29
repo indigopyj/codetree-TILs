535b13dc30cb9df3140550e7659f9811e6b47879
Yeojeong Park <indigopyj@gmail.com>
2024-09-29 21:22:48 +0900

    Title: 코드트리 빵, Time: 410ms, Memory: 27MB, Status: Passed - CodeTree

--- 240929/코드트리 빵/codetree-mon-bread.py
+++ 240929/코드트리 빵/codetree-mon-bread.py
@@ -25,26 +25,28 @@ def move_shortest(p_x, p_y, t_x, t_y):
     visited = [[False] * n for _ in range(n)]
     q.append((p_x, p_y, []))
     visited[p_x][p_y] = True
+    # (2,0) ( 0,2)
     while q:
         x, y, d_list = q.popleft()
         if (x,y) == (t_x, t_y):
-            break
+            final_dir = dirs[d_list[0]]
+            nx, ny = p_x + final_dir[0], p_y + final_dir[1]
+            return nx, ny, len(d_list)
         for i, d in enumerate(dirs):
             dx, dy = x + d[0], y + d[1]
             if 0 <= dx < n and 0 <= dy < n and grid[dx][dy] != -1 and not visited[dx][dy]:
+                # print(p_x, p_y, dx, dy)
                 visited[dx][dy] = True
                 q.append((dx, dy, d_list+[i]))
-    # print(d_list)
     # print()
-    final_dir = dirs[d_list[0]]
-    nx, ny = p_x + final_dir[0], p_y + final_dir[1]
-    return nx, ny, len(d_list)
+    # 길이 없다
+    return p_x, p_y, 99999999999+1
 
 
 def move(people):
     end_stores = []
     new_people = []
-
+    # print(people)
     for p_id, (p_x, p_y) in people.items():
         if (p_x, p_y) == (-1, -1):
             continue
@@ -64,12 +66,12 @@ def move_to_base(store):
     sx, sy = store
     min_dist = 99999999999
     tx, ty = 20, 20
-    # for r in grid:
-    #     print(r)
-    # print()
+    
+    # print(base_camps, store)
     for cx, cy in base_camps:
+        # print(sx, sy, cx, cy)
         _, _, dist = move_shortest(sx, sy, cx, cy)
-        # print("base info", cx, cy, dist)
+        # print("base info", sx, sy, cx, cy, dist)
         if min_dist >= dist:
             if min_dist == dist:
                 if tx == cx:
