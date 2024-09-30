54754eaf6695d7677c99a9d152d476c91dffd7ac
Yeojeong Park <indigopyj@gmail.com>
2024-09-30 22:29:17 +0900

    Title: 산타의 선물 공장, Time: 5035ms, Memory: 60MB, Status: Time Limit Exceed - CodeTree

--- 240930/산타의 선물 공장/santa-gift-factory.py
+++ 240930/산타의 선물 공장/santa-gift-factory.py
@@ -3,6 +3,7 @@ q = int(input())
 
 belts = dict()
 boxes = dict()
+weights = dict()
 for _ in range(q):
     inputline = list(map(int, input().split()))
     cmd = inputline[0]
@@ -10,16 +11,18 @@ for _ in range(q):
     if cmd == 100:
         n, m = inputline[1:3]
         ids = inputline[3:3+n]
-        weights = inputline[3+n:]
+        ws = inputline[3+n:]
         # print(len(ids), len(weights))
         for i in range(1,m+1):
             belts[i] = []
-        for i, (box_id, w) in enumerate(zip(ids, weights)):
+        for i, (box_id, w) in enumerate(zip(ids, ws)):
             b_id = i // (n//m) + 1
-            belts[b_id].append((box_id, w))
+            belts[b_id].append(box_id)
+            weights[box_id] = w
             boxes[box_id] = b_id
         # print(belts)
         # print(boxes)
+        # print(weights)
 
     elif cmd == 200: # box out
         w_max = inputline[1]
@@ -28,28 +31,30 @@ for _ in range(q):
             # print(b_id, belt)
             if belt is None or len(belt) == 0:
                 continue
-            if belt[0][1] > w_max:
+            
+            if weights[belt[0]] > w_max:
                 item = belt[0]
                 belt.append(item)
             else:
-                total_sum += belt[0][1]
+                total_sum += weights[belt[0]]
                 # print("pop box", belt[0])
-                boxes[belt[0][0]] = -1
+                del boxes[belt[0]]
             del belt[0]
         print(total_sum)
     elif cmd == 300: # remove box
         r_id = inputline[1]
-        if r_id not in boxes or boxes[r_id] == -1:
+        if r_id not in boxes:
             print(-1)
             continue
         belt_id = boxes[r_id]
         new_belt = []
-        for b in belts[belt_id]:
-            if b[0] == r_id:
-                continue
-            new_belt.append(b)
-        belts[belt_id] = new_belt
-        boxes[r_id] = -1
+        # for b in belts[belt_id]:
+        #     if b == r_id:
+        #         continue
+        #     new_belt.append(b)
+        # belts[belt_id] = new_belt
+        belts[belt_id].remove(r_id)
+        del boxes[r_id]
         print(r_id)
 
     elif cmd == 400: # check box
@@ -59,12 +64,13 @@ for _ in range(q):
             continue
         belt_id = boxes[f_id]
         print(belt_id)
-        if belt_id != -1 and belts[belt_id] is not None:
-            new_belt = []
-            idx = 0
-            for i, (box_id, _) in enumerate(belts[belt_id]):
-                if box_id == f_id:
-                    idx = i
+        if belts[belt_id] is not None:
+            # new_belt = []
+            # idx = 0
+            idx = belts[belt_id].index(f_id)
+            # for i, (box_id, _) in enumerate(belts[belt_id]):
+            #     if box_id == f_id:
+            #         idx = i
             new_belt = belts[belt_id][idx:] + belts[belt_id][:idx]
             belts[belt_id] = new_belt
             # print(new_belt)
@@ -86,7 +92,7 @@ for _ in range(q):
         # print(belts[new_b_id])
         belts[new_b_id] = belts[new_b_id] + tmp
         for b in tmp:
-            boxes[b[0]] = new_b_id
+            boxes[b] = new_b_id
         print(b_num)
         # print(belts[new_b_id])
         # print(boxes)
