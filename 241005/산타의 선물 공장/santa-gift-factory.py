f7d623acb185ca18343c6523f78d43a12392bb78
Yeojeong Park <indigopyj@gmail.com>
2024-10-05 21:58:26 +0900

    Title: 산타의 선물 공장, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241005/산타의 선물 공장/santa-gift-factory.py
+++ 241005/산타의 선물 공장/santa-gift-factory.py
@@ -29,6 +29,35 @@ def push_head(b_id, item):
     
     return
 
+def push_item(b_id, item):
+    old_tail = tails[b_id]
+    backs[old_tail] = item
+    fronts[item] = old_tail
+    tails[b_id] = item
+    which_belt[item] = b_id
+
+
+
+def remove_item(box_i):
+    belt_i = which_belt[box_i]
+    
+    front = fronts[box_i]
+    back = backs[box_i]
+    if heads[belt_i] == tails[belt_i]:
+        heads[belt_i] = tails[belt_i] = 0
+    elif heads[belt_i] == box_i:
+        heads[belt_i] = back
+        backs[box_i] = 0
+    elif tails[belt_i] == box_i:
+        tails[belt_i] = front
+        fronts[box_i] = 0
+    else:
+        fronts[back] = front
+        backs[front] = back
+
+    which_belt[box_i] = -1
+    fronts[box_i] = backs[box_i] = 0
+
 for _ in range(q):
     line = list(map(int, input().split()))
     # print(line)
@@ -81,17 +110,14 @@ for _ in range(q):
                 continue
             head_weight = weights[heads[i]]
             if head_weight <= w_max:
-                item = pop_head(i)
+                item = remove_item(heads[i])
                 # print("here")
                 # print(heads, tails)
                 total_sum += weights[item]
             elif heads[i] != tails[i]:
-                head = pop_head(i)
-                which_belt[head] = i
-                old_tail = tails[i]
-                fronts[head] = old_tail
-                backs[old_tail] = head
-                tails[i] = head
+                head = heads[i]
+                remove_item(heads[i])
+                push_item(i, head)
                 # print("there")
                 # print(heads, tails)
         print(total_sum)
@@ -108,18 +134,8 @@ for _ in range(q):
             print(-1)
             continue
         print(r_id)
-        which_belt[box_i] = -1
-        front = fronts[box_i]
-        back = backs[box_i]
-        if heads[belt_i] == box_i:
-            heads[belt_i] = back
-        if tails[belt_i] == box_i:
-            tails[belt_i] = front
-        if front != 0:
-            backs[front] = back
-        if back != 0:
-            fronts[back] = front
-        fronts[box_i] = backs[box_i] = 0
+        remove_item(box_i)
+
         # print(i_to_iid)
         # print(heads, tails)
         # print(fronts, backs)
@@ -145,7 +161,7 @@ for _ in range(q):
             tails[belt_i] = new_tail
             push_head(belt_i, box_i)
         else:
-            new_tail = fronts[origin_tail]
+            new_tail = fronts[box_i]
             fronts[origin_head] = origin_tail
             backs[origin_tail] = origin_head
 
