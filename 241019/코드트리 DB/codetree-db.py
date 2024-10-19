4e1b2c6c25ffb46d219e1380690511b38b4f7d32
Yeojeong Park <indigopyj@gmail.com>
2024-10-19 16:49:51 +0900

    Title: 코드트리 DB, Time: 8074ms, Memory: 50MB, Status: Time Limit Exceed - CodeTree

--- 241019/코드트리 DB/codetree-db.py
+++ 241019/코드트리 DB/codetree-db.py
@@ -1,98 +1,111 @@
-import bisect
+class Node:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.sum = 0
+        self.left = None
+        self.right = None
 
-class Node():
-    def __init__(self, is_end=False):
-        self.value = 0
-        self.is_end = is_end
-        self.children = dict()
 
-class Trie():
-    def __init__(self):
-        self.root = Node() # root node
-        self.values = []
+class Tree:
+    def __init__(self, start, end):
+        self.root = Node(start, end)
         
-    def insert(self, name, value):
-        for n, v in self.values:
-            if n == name or v == value:
-                return 0
-        curr = self.root
-        for char in name:
-            if char not in curr.children:
-                curr.children[char] = Node()
-            curr = curr.children[char]
-        curr.is_end = True
-        curr.value = value
-        self.values.append((name, value))
-        # self.values.sort(key=lambda x:x[1])
-        bisect.insort(self.values, (name, value))
-        return 1
-    
-    def delete(self, name):
-        curr = self.root
-        node = []
-        for char in name:
-            if char in curr.children:
-                node.append((curr, char)) 
-                curr = curr.children[char]
-            else:
-                return 0
-        if curr.is_end:
-            value = curr.value
-            curr.is_end = False
-            curr.value = 0
-            index = bisect.bisect_left(self.values, (name, 0))
-            if index < len(self.values) and self.values[index][0] == name:
-                self.values.pop(index)
-            # self.values = [item for item in self.values if item[0] != name]
-            for n, c in node[::-1]: # 맨마지막노드는 포함안된 상태
-                if not curr.is_end and len(curr.children) == 0:
-                    del n.children[c]
-                    curr = n
-                else:
-                    break
-            return value
-        return 0
-                    
-    def rank(self, k):
-        if k <= len(self.values):
-            return self.values[k-1][0]
-        return None
-    
-    def sum(self, k):
+    def update(self, node, idx, value):
+        
+        if node.start == node.end:
+            node.sum = value
+            return
+        
+        mid = (node.start + node.end) // 2
+        if idx <= mid:
+            if node.left is None:
+                node.left = Node(node.start, mid)
+            self.update(node.left, idx, value)
+        else:
+            if node.right is None:
+                node.right = Node(mid+1, node.end)
+            self.update(node.right, idx, value)
+        
+        node.sum = 0
+        if node.right is not None:
+            node.sum += node.right.sum
+        if node.left is not None:
+            node.sum += node.left.sum
+        
+    def range_sum(self, node, left, right):
+        
+        if node is None or node.end < left or node.start > right:
+            return 0
+        
+        if node.start >= left and node.end <= right:
+            return node.sum
+        
         total = 0
-        for n, v in self.values:
-            if v <= k:
-                total += v
-            else:
-                break
+        if node.left:
+            total +=  self.range_sum(node.left, left, right)
+            ##print("left : ", total)
+        if node.right:
+            total +=  self.range_sum(node.right, left, right)
+            #print("right : ", total)
         return total
+
+def insert(name, value):
+    for n, v in data_info.items():
+        if name == n:
+            return 0
+        if value == v:
+            return 0
     
-    def init(self):
-        self.root = Node() # root node
-        self.values = []
-            
-        
+    data_info[name] = value
+    tree.update(tree.root, value, value)
+    return 1
+    
+def delete(name):
+    flag = False
+    value = 0
+    for n, v in data_info.items():
+        if name == n:
+            flag = True
+            value = v
+            break
+    if not flag:
+        return 0
+    del data_info[name]
+    tree.update(tree.root, value, 0)
+    return value
+
+def rank(k):
+    if k > len(data_info):
+        return None
+    sort_dict = sorted(data_info.items(), key=lambda x:x[1])
+    k, v = sort_dict[k-1]
+    return k
+
+def sum(k):
+    return tree.range_sum(tree.root, 0, k)
 
 Q = int(input())
-trie = Trie()
+
 for _ in range(Q):
     line = input().split()
     cmd = line[0]
     
     if cmd == 'init':
-        trie.init()
+        tree = Tree(start=0, end=10**9)
+        data_info = dict()
         
     elif cmd == 'insert':
         name, value = line[1], int(line[2])
-        print(trie.insert(name, value))
-        
+        print(insert(name, value))
+                
     elif cmd == 'delete':
         name = line[1]
-        print(trie.delete(name))
+        print(delete(name))
     elif cmd == 'rank':
         k = int(line[1])
-        print(trie.rank(k))
+        print(rank(k))
             
     elif cmd == 'sum':
         k = int(line[1])
-        print(trie.sum(k))
+        print(sum(k))
