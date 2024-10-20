c12fc087818489a8d36c783437f981454b97db3a
Yeojeong Park <indigopyj@gmail.com>
2024-10-20 17:42:20 +0900

    Title: 코드트리 오마카세, Time: 850ms, Memory: 73MB, Status: Wrong Answer - CodeTree

--- 241020/코드트리 오마카세/codetree-omakase.py
+++ 241020/코드트리 오마카세/codetree-omakase.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Query:
     def __init__(self, cmd, t, x=-1, name=None, n=-1):
         self.cmd = cmd
@@ -8,10 +9,11 @@ class Query:
 
 L, Q = map(int, input().split())
 queries = []
-sushi_queries = []
+sushi_queries = defaultdict(list)
 people_in = dict()
 people_pos = dict()
 people_out = dict()
+names = []
 for _ in range(Q):
     line = input().split()
     cmd = int(line[0])
@@ -19,7 +21,7 @@ for _ in range(Q):
         t, x = map(int, line[1:-1])
         name = line[-1]
         queries.append(Query(cmd, t, x, name))
-        sushi_queries.append(Query(cmd, t, x, name))
+        sushi_queries[name].append(Query(cmd, t, x, name))
     elif cmd == 200:
         t, x = map(int, line[1:3])
         name = line[3]
@@ -27,28 +29,29 @@ for _ in range(Q):
         queries.append(Query(cmd, t, x, name, n))
         people_in[name] = t
         people_pos[name] = x
+        names.append(name)
     elif cmd == 300:
         t = int(line[1])
         queries.append(Query(cmd, t))
 
-for q in sushi_queries:
-    name = q.name
-    if name not in people_out:
-        people_out[name] = 0
-    p_t = people_in[name]
-    p_x = people_pos[name]
-    s_t = q.t
-    s_x = q.x
-    if p_t < s_t: # 사람이 먼저 도착
-        eat_time = s_t + (p_x - s_x + L) % L
-        
-    else: # 사람이 나중에 도착
-        moved_dist = (p_t - s_t)
-        need_time = p_x - (s_x + moved_dist) % L
-        eat_time = p_t + need_time
-    #print(name, eat_time)
-    queries.append(Query(111, eat_time, name=name))
-    people_out[name] = max(people_out[name], eat_time)
+for name in names:
+    people_out[name] = 0
+    for q in sushi_queries[name]:
+            
+        p_t = people_in[name]
+        p_x = people_pos[name]
+        s_t = q.t
+        s_x = q.x
+        if p_t < s_t: # 사람이 먼저 도착
+            eat_time = s_t + (p_x - s_x + L) % L
+            
+        else: # 사람이 나중에 도착
+            moved_dist = (p_t - s_t)
+            need_time = p_x - (s_x + moved_dist) % L
+            eat_time = p_t + need_time
+        #print(name, eat_time)
+        queries.append(Query(111, eat_time, name=name))
+        people_out[name] = max(people_out[name], eat_time)
 
 for name, time in people_out.items():
     queries.append(Query(222, time, name=name))
